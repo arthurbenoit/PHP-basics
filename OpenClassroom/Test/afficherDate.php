3c57b2e647f6e3740a69b2c6363967a012d4e027
Arthur <agencecoupdepouce@gmail.com>
2018-01-15 23:07:43 +0100

    Dossier test OpenClassroom

---
+++ OpenClassroom/Test/afficherDate.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page web</title>
+</head>
+<body>
+<h1>Ma page web</h1>
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+</body>
+</html>
+
+<?php
+    echo "Cette date change à chaque rafraîchissement de la page";
+?>
